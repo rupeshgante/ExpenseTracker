1cb4ec99da1a304fe6d1b0c33e067aeb1eaeded0
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-08-09 17:06:27 +0530

    added mailing service

--- BACK-END/controllers/password.js
+++ BACK-END/controllers/password.js
@@ -1,11 +1,26 @@
 const User=require('../models/user');
 const Expense=require('../models/expense');
-const bcrypt=require('bcrypt');
-const jwt = require('jsonwebtoken');
+const sendmail=require('@sendgrid/mail');
+
+sendmail.setApiKey(process.env.SENGRID_API_KEY);
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 exports.forgotPassword=(req,res)=>{
-    console.log('receiveed');
-    res.json('received');
+   const mail=req.body.email;
+   console.log(mail);
+    const message={
+    to:mail,
+    from:'rupeshgante@gmail.com',
+    subject:'reset password link',
+    text:'Reset your password here'
+   }
+   sendmail.send(message)
+      .then(response=>{
+        res.status(200).json('Reset link sent to email')
+      })
+      .catch(err=>{
+        res.status(400).json(err);
+      })
 }
--- FRONT-END/login/login.js
+++ FRONT-END/login/login.js
@@ -2,9 +2,12 @@ const form=document.getElementById('form');
 
 form.addEventListener('click',(e)=>{
     e.preventDefault();
-    if(e.target.className=='login'){
-    const email=form.email.value;
-    const password=form.password.value;
+
+    //login
+
+    if(e.target.className=='login-button'){
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
 
     axios.post('http://localhost:7000/login',{
         email:email,
@@ -29,12 +32,24 @@ form.addEventListener('click',(e)=>{
         }
     })
 }
+//FORGOT password
+
 if(e.target.className=='forgot-password'){
     console.log('clicked');
     document.querySelector('#forgot').style = "display:block;"
+    document.querySelector('#login-form').style = "display:none;"
+
 }
 if(e.target.className=='reset'){
-    axios.post('http://localhost:7000/forgotpassword',{})
+    const mail=document.getElementById('reset-email').value;
+    console.log(mail);
+    axios.post('http://localhost:7000/forgotpassword',{email:mail})
+    .then(res=>{
+        console.log(res);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 
 }
 })
